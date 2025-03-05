691277386796920d11543aa35ce50db912eeb113
“Astrolithia” <astrolithia@icloud.com>
2025-03-05 10:56:55 +0800

    后端创建

---
+++ mall-shopping-backend/mall-shopping/src/test/java/com/qvtu/mallshopping/MallShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qvtu.mallshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
